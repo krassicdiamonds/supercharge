47e5d52528705a842fa2aebf00668a32317cdc9a
Tech With Ulunji <techwithulunji@gmail.com>
2026-05-28 21:22:51 +0200

    Defined register, login and logout routes. Also included routes for the system dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome')->name('welcome');
+
+// logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// guest routes
+Route::middleware('guest')->controller(AuthController::class)->group(function(){
+    // handle get request for register page
+    Route::get('/register', 'showRegister')->name('show.register');
+    // handle get request for login page
+    Route::get('/login', 'showLogin')->name('show.login');
+
+    // handle post request for register
+    Route::post('/register', 'register')->name('register');
+    // handle post request for login
+    Route::post('/login', 'login')->name('login');
+});
+
+// auth routes
+Route::middleware('auth')->group(function(){
+    Route::get('/dashboard/home', function(){
+        return view('dashboard.home');
+    })->name('dashboard.home');;
 });
