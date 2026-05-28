8378c30b82d45e2a7869f15e75d77b4f2a7dd883
Tech With Ulunji <techwithulunji@gmail.com>
2026-05-28 22:01:19 +0200

    Added response design to the UI

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -9,31 +9,31 @@
 </head>
 <body class="bg-white">
     {{-- header --}}
-    <header class="top-0 sticky px-40 py-5">
+    <header class="top-0 sticky px-5 sm:px-10 md:px-20 lg:px-40 py-5">
         <div class="flex justify-between items-center">
             <a href="{{ route('welcome') }}" class="flex items-center gap-x-1">
                 {{-- icon --}}
-                <div class="p-1 border border-yellow-100 rounded-full">
-                    <svg class="size-6 text-yellow-200" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.69667 0.0403541C8.90859 0.131038 9.03106 0.354857 8.99316 0.582235L8.0902 6.00001H12.5C12.6893 6.00001 12.8625 6.10701 12.9472 6.27641C13.0319 6.4458 13.0136 6.6485 12.8999 6.80001L6.89997 14.8C6.76167 14.9844 6.51521 15.0503 6.30328 14.9597C6.09135 14.869 5.96888 14.6452 6.00678 14.4178L6.90974 9H2.49999C2.31061 9 2.13748 8.893 2.05278 8.72361C1.96809 8.55422 1.98636 8.35151 2.09999 8.2L8.09997 0.200038C8.23828 0.0156255 8.48474 -0.0503301 8.69667 0.0403541ZM3.49999 8.00001H7.49997C7.64695 8.00001 7.78648 8.06467 7.88148 8.17682C7.97648 8.28896 8.01733 8.43723 7.99317 8.5822L7.33027 12.5596L11.5 7.00001H7.49997C7.353 7.00001 7.21347 6.93534 7.11846 6.8232C7.02346 6.71105 6.98261 6.56279 7.00678 6.41781L7.66968 2.44042L3.49999 8.00001Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                <div class="p-0.5 sm:p-1 border border-yellow-100 rounded-full">
+                    <svg class="size-4 sm:size-6 text-yellow-200" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.69667 0.0403541C8.90859 0.131038 9.03106 0.354857 8.99316 0.582235L8.0902 6.00001H12.5C12.6893 6.00001 12.8625 6.10701 12.9472 6.27641C13.0319 6.4458 13.0136 6.6485 12.8999 6.80001L6.89997 14.8C6.76167 14.9844 6.51521 15.0503 6.30328 14.9597C6.09135 14.869 5.96888 14.6452 6.00678 14.4178L6.90974 9H2.49999C2.31061 9 2.13748 8.893 2.05278 8.72361C1.96809 8.55422 1.98636 8.35151 2.09999 8.2L8.09997 0.200038C8.23828 0.0156255 8.48474 -0.0503301 8.69667 0.0403541ZM3.49999 8.00001H7.49997C7.64695 8.00001 7.78648 8.06467 7.88148 8.17682C7.97648 8.28896 8.01733 8.43723 7.99317 8.5822L7.33027 12.5596L11.5 7.00001H7.49997C7.353 7.00001 7.21347 6.93534 7.11846 6.8232C7.02346 6.71105 6.98261 6.56279 7.00678 6.41781L7.66968 2.44042L3.49999 8.00001Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
                 </div>
-                <h2 class="font-semibold capitalize">supercharge</h2>
+                <h2 class="font-semibold text-xs sm:text-sm capitalize">supercharge</h2>
             </a>
 
-            <div class="flex items-center gap-x-5">
+            <div class="flex items-center gap-x-2.5 sm:gap-x-5">
 
-                <a class="hover:shadow px-5 py-1 border border-gray-500 rounded-sm text-sm capitalize transition-shadow duration-150" href="{{ route('show.login') }}">login</a>
+                <a class="hover:shadow px-2.5 sm:px-5 py-0.5 sm:py-1 border border-gray-500 rounded-sm text-xs sm:text-sm capitalize transition-shadow duration-150" href="{{ route('show.login') }}">login</a>
 
-                <a class="bg-black hover:shadow px-5 py-1 rounded-sm text-white text-sm capitalize transition-shadow duration-150" href="{{ route('show.register') }}">register</a>
+                <a class="bg-black hover:shadow px-2.5 sm:px-5 py-0.5 sm:py-1 rounded-sm text-white text-xs sm:text-sm capitalize transition-shadow duration-150" href="{{ route('show.register') }}">register</a>
 
             </div>
         </div>
     </header>
     {{-- main --}}
-    <main class="px-40">
+    <main class="px-5 sm:px-20 md:px-40">
         {{ $slot }}
     </main>
     {{-- footer --}}
-    <footer class="">
+    <footer class="px-5 sm:px-20 md:px-40">
         <h3 class="text-gray-500 text-xs text-center">&copy; Supercharge team</h3>
     </footer>
 </body>
