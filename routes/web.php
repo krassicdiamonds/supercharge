a1cdb4c44dce15c149245ac5b38c6ae5de99a135
Tech With Ulunji <techwithulunji@gmail.com>
2026-05-29 14:16:27 +0200

    Added GET request handlers for all internal screens only vissible to authorized users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NavigationController;
+use App\Http\Controllers\TaskManagementController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('welcome');
@@ -9,7 +11,7 @@ Route::view('/', 'welcome')->name('welcome');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // guest routes
-Route::middleware('guest')->controller(AuthController::class)->group(function(){
+Route::middleware(['guest'])->controller(AuthController::class)->group(function(){
     // handle get request for register page
     Route::get('/register', 'showRegister')->name('show.register');
     // handle get request for login page
@@ -21,9 +23,27 @@ Route::middleware('guest')->controller(AuthController::class)->group(function(){
     Route::post('/login', 'login')->name('login');
 });
 
-// auth routes
-Route::middleware('auth')->group(function(){
-    Route::get('/dashboard/home', function(){
-        return view('dashboard.home');
-    })->name('dashboard.home');;
+
+// Return views via NavigationController
+Route::middleware(['auth'])->prefix('dashboard')->controller(NavigationController::class)->group(function(){
+    // handle get home request
+    Route::get('/home', 'showHome')->name('dashboard.home');
+    // handle get dashboard request
+    Route::get('/dashboard', 'showDashboard')->name('dashboard.dashboard');
+    // handle get teams request
+    Route::get('/teams', 'showTeams')->name('dashboard.teams');
+    // handle get boards request
+    Route::get('/boards', 'showBoards')->name('dashboard.boards');
+    // handle get inbox view
+    Route::get('/inbox', 'showInbox')->name('dashboard.inbox');
+    // handle get timeline request
+    Route::get('/timeline', 'showTimeline')->name('dashboard.timeline');
+    // handle get settings request
+    Route::get('/settings', 'showSettings')->name('dashboard.settings');
+});
+
+// Get Data from the Task Management Controller
+
+Route::middleware(['auth'])->controller(TaskManagementController::class)->group(function(){
+
 });
