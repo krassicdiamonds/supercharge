26d68fa7bcef15749a14ea47c21dd0ba4667aada
Tech With Ulunji <techwithulunji@gmail.com>
2026-05-29 22:03:09 +0200

    Moved logout button to settings

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,12 +1,9 @@
 <x-layouts.auth>
 <div>
-<h2>
-    {{ Auth::user()->name }} Welcome to the dashboard!
+<h2 class="text-center">
+    <span class="font-bold">Hello {{ Auth::user()->name }}!</span> Welcome to the Supercharge
 </h2>
 
-<form action="{{ route('logout') }}" method="POST">
-    @csrf
-    <button class="bg-black hover:shadow px-5 py-1 rounded-sm text-white text-sm capitalize transition-shadow duration-150" type="submit">Logout</button>
-</form>
+
 </div>
 </x-layouts>
--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -1,3 +1,9 @@
 <x-layouts.auth>
     <h3 class="text-center">settings</h3>
+    <div class="text-center">
+        <form action="{{ route('logout') }}" method="POST">
+        @csrf
+        <button class="bg-black hover:shadow px-5 py-1 rounded-sm text-white text-sm capitalize transition-shadow duration-150 cursor-pointer" type="submit">Logout</button>
+    </div>
+</form>
 </x-layouts>
